744400fa4ede1d72e5106996a5da767dc4156c12
John Desrosiers <john.desro@gmail.com>
2020-05-08 14:52:31 -0700

    Updates

---
+++ precache-manifest.3fc7e467ba7a52418368e1839bfc26dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2894c5b1900535d1efcc248989c4c0f0",
+    "url": "/harry-potter-table-demo/index.html"
+  },
+  {
+    "revision": "8f74c975b3b71d24013c",
+    "url": "/harry-potter-table-demo/static/js/2.ab32c822.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/harry-potter-table-demo/static/js/2.ab32c822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "165e527f4c6567f0bfae",
+    "url": "/harry-potter-table-demo/static/js/main.998e40b0.chunk.js"
+  },
+  {
+    "revision": "d92441376b54879e55cc",
+    "url": "/harry-potter-table-demo/static/js/runtime-main.8cf19692.js"
+  }
+]);
